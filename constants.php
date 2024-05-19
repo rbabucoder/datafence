dc5a537d81a59db5d5b2697ec0dad0a23c957b6c
unknown <rbabu310197@gmail.com>
2024-05-19 12:28:22 +0530

    footer form issue fix

--- constants.php
+++ constants.php
@@ -3,7 +3,7 @@
 // constants.php
 
 // Define global constant for image directory path
-define('ASSET_VERSION', '1.0.14');
+define('ASSET_VERSION', '1.0.15');
 define('IMAGE_DIR', get_template_directory_uri() . '/images');
 define('CSS_DIR', get_template_directory_uri() . '/css');
 define('JS_DIR', get_template_directory_uri() . '/js');
