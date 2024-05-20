46bce6488f39f56faf3e7c65af938f617f041e9f
unknown <rbabu310197@gmail.com>
2024-05-20 15:18:04 +0530

    version update

--- constants.php
+++ constants.php
@@ -3,7 +3,7 @@
 // constants.php
 
 // Define global constant for image directory path
-define('ASSET_VERSION', '1.0.24');
+define('ASSET_VERSION', '1.0.25');
 define('IMAGE_DIR', get_template_directory_uri() . '/images');
 define('CSS_DIR', get_template_directory_uri() . '/css');
 define('JS_DIR', get_template_directory_uri() . '/js');
