46755d8b8a54e41c517f6bae331409ac5ef6da34
unknown <rbabu310197@gmail.com>
2024-05-27 23:28:31 +0530

    add new section in homepage

--- constants.php
+++ constants.php
@@ -3,7 +3,7 @@
 // constants.php
 
 // Define global constant for image directory path
-define('ASSET_VERSION', '1.0.38');
+define('ASSET_VERSION', '1.0.40');
 define('IMAGE_DIR', get_template_directory_uri() . '/images');
 define('CSS_DIR', get_template_directory_uri() . '/css');
 define('JS_DIR', get_template_directory_uri() . '/js');
